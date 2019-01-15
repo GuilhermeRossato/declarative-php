adbd7990135a20f3346687bf63dcab0659e2156b
Guilherme Rossato <guilhermrossato@gmail.com>
2019-01-15 21:29:55 -0200

    Added Page Class

---
+++ src/Rossato/Page.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Rossato;
+
+use Rossato/Elements;
+
+/**
+ *	A class used to represent an HTML page
+ */
+
+class Page extends Element {
+	public function __construct($content = []) {
+		$this->head = new Element("head");
+		$this->body = new Element("body", [], $content);
+		super::construct("html", [], [$head, $body]);
+	}
+
+	/**
+	 * Transform the element and its content into raw HTML code
+	 * @return string The compressed HTML code that represents the object and its content
+	 */
+	public function flatten() {
+		$content = $this->getHeader();
+
+		if ($this->tag === "img" || $this->tag === "input") {
+			return $content;
+		}
+
+		foreach ((is_array($this->content) ? $this->content : [$this->content]) as $part) {
+			$content .= ($part instanceof HTMLElement)?$part->flatten():$part;
+		}
+
+		$content .= "</".$this->tag.">";
+
+		return "<!doctype html>".$content;
+	}
+
+	public function __toString() {
+		return $this->flatten();
+	}
+}

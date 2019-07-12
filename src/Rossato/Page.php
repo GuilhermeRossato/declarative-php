e0bf762ffd398921f48b718ffa83330b7e9a53f8
Guilherme Rossato <guilhermrossato@gmail.com>
2019-07-11 21:52:57 -0300

    Simplified Page class to avoid code repetition
    Just so I can achieve 100% code coverage on my tests

--- src/Rossato/Page.php
+++ src/Rossato/Page.php
@@ -9,10 +9,10 @@ use Rossato\Element;
  */
 
 class Page extends Element {
-	public function __construct($content = []) {
+	public function __construct($content = [], $htmlConfig = [], $bodyConfig = []) {
 		$this->head = new Element("head");
-		$this->body = new Element("body", [], $content);
-		parent::__construct("html", [], [$this->head, $this->body]);
+		$this->body = new Element("body", $bodyConfig, $content);
+		parent::__construct("html", $htmlConfig, [$this->head, $this->body]);
 	}
 
 	/**
@@ -20,22 +20,6 @@ class Page extends Element {
 	 * @return string The compressed HTML code that represents the object and its content
 	 */
 	public function flatten() {
-		$content = $this->getHeader();
-
-		if ($this->tag === "img" || $this->tag === "input") {
-			return $content;
-		}
-
-		foreach ((is_array($this->content) ? $this->content : [$this->content]) as $part) {
-			$content .= ($part instanceof HTMLElement)?$part->flatten():$part;
-		}
-
-		$content .= "</".$this->tag.">";
-
-		return "<!doctype html>".$content;
-	}
-
-	public function __toString() {
-		return $this->flatten();
+		return "<!doctype html>".parent::flatten();
 	}
 }

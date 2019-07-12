4967c76da3f03b971f0ed614fa48d17d7e8201cd
Guilherme Rossato <guilhermrossato@gmail.com>
2019-07-11 23:20:16 -0300

    Added new basic test to test custom elements (with dashes)

--- tests/basicFunctionalityTest.php
+++ tests/basicFunctionalityTest.php
@@ -17,7 +17,7 @@ class BasicFunctionalityTest extends TestCase {
 		$this->assertSame(
 			"<div></div>",
 			$div->render(),
-			"Got unexpected render result from div string"
+			"Got unexpected render result from div as string"
 		);
 	}
 
@@ -39,13 +39,23 @@ class BasicFunctionalityTest extends TestCase {
 		$this->assertSame(
 			$expectedPageString,
 			$page->__toString(),
-			"Got unexpected string result from page string"
+			"Got unexpected string result from page as string"
 		);
 
 		$this->assertSame(
 			$expectedPageString,
 			$page->render(),
-			"Got unexpected string result from page string"
+			"Got unexpected string result from page as string"
+		);
+	}
+
+	public function testCustomHtmlElement() {
+		$element = new Element("my-element", ["class" => "root"]);
+
+		$this->assertSame(
+			'<my-element class="root"></my-element>',
+			(string) $element,
+			"Got unexpected string result from element as string"
 		);
 	}
 
